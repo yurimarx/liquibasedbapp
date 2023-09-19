3a76348381b7f5520ff05201766a242346b4d473
yurimarx <yurimarx@gmail.com>
2023-09-19 09:14:39 -0300

    initial version

---
+++ src/main/java/com/fiserv/liquibase/LiquibasedbappApplication.java
@@ -0,0 +1,13 @@
+package com.fiserv.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibasedbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibasedbappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiserv/liquibase/LiquibasedbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiserv.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibasedbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
